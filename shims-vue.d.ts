f85728946091ae9d66fb09564b37b8bd24dd9a50
yanyongxing <1959555480@qq.com>
2022-05-18 18:12:47 +0800

    vue3+自动格式化

---
+++ shims-vue.d.ts
@@ -0,0 +1,13 @@
+// 声明vue文件的导出类型
+declare module "*.vue" {
+  import { defineComponent } from "vue"
+  const component: ReturnType<typeof defineComponent>
+  export default component
+}
+
+// 声明全局属性类型
+declare module "@vue/runtime-core" {
+  interface ComponentCustomProperties<T> {
+    $api: T
+  }
+}
